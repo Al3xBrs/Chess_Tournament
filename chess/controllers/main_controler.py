29d037c5295b6362e9f3149b1336ae878d93a879
AlexandreGazagnes <a_syoez@yahoo.fr>
2023-03-13 17:01:10 +0100

    upate controlers

--- chess/controllers/main_controler.py
+++ chess/controllers/main_controler.py
@@ -1,16 +1,20 @@
 from chess.controllers.players_controller import *
-from chess.controllers.tournaments import *
+
 from chess.views.main_view import *
 
 
 def main_menu_controller():
-    """ """
+    """main_menu_controller"""
 
     choice = main_menu_view()
 
-    if choice == 1:
+    if choice == "1":
         return players_menu_controler()
-    elif choice == 2:
-        return tournaments_menu_controler()
-    elif choice == 4:
-        return main_menu_controller()
+    elif choice == "2":
+        pass
+    elif choice == "4":
+        pass
+    elif choice == "q":
+        raise KeyboardInterrupt("Bye Bye")
+
+    return main_menu_controller()
--- chess/controllers/players_controller.py
+++ chess/controllers/players_controller.py
@@ -1,17 +1,20 @@
 from chess.views.player_view import *
 from chess.views.main_view import *
-from chess.views.tournament_view import *
+
 from chess.models.players import Player
-from chess.controllers.tournaments import *
+
 from chess.controllers.main_controler import *
 
-table = Player.find_all()
+
+# table = Player.find_all()
 
 
 # TODO : Trier les listes d'instance
 
 
 def players_menu_controler():
+    """ """
+
     choice = players_menu_view()
 
     if choice == 1:
@@ -19,12 +22,21 @@ def players_menu_controler():
     elif choice == 2:
         return player_create_menu_controler()
     elif choice == 4:
-        return main_menu_controller()
+        pass
+
+    return main_menu_controller()
 
 
 def players_list_menu_controler():
+    """ """
+
     choice = players_list_menu_view()
 
+    # query ensemble des datas de player
+    # deserialise la liste des players (list de ditc)
+    # map choice vs clé 1/alpha, 2/ine etc etc
+
+    # tri de la liste en fonction de la clé
     if choice == 1:
         pass
     elif choice == 2:
@@ -34,7 +46,24 @@ def players_list_menu_controler():
     elif choice == 4:
         return players_menu_controler()
 
+    # call players_list_view()
+
+    # player_submenu_view()
+
+    # soit player_update_view
+    # soit player_delete_view
+
+    # go back player menu generique
+
 
 def player_create_menu_controler():
+    """ """
+
     choice = player_create_menu_view()
-    player = Player(choice)
+
+    # transform string to dict
+
+    player = Player(**choice)
+    player.create()
+
+    # go back player menu

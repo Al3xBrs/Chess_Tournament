d5a5fea1ccd67312a6f2af88b4b78e36dd2d73af
Alex Brs <alexbrise.40@gmail.com>
2023-03-20 11:04:42 +0100

    First models tournaments

--- chess/models/tournaments.py
+++ chess/models/tournaments.py
@@ -1,13 +1,66 @@
 
 class Tournament:
-    def __init__(self, rounds_number):
-        self.rounds_number = rounds_number
+    MAX_PLAYERS = 4
+    DATA_TOURNAMENTS =""
+    def __init__(
+            self,
 
-    def create(tournament):
+            nom,
+            lieu = "",
+            date_debut = "",
+            date_fin = "",
+            nombre_tours = 4,
+            numero_tour = -1,
+            liste_tours = [],
+            liste_joueurs = [],
+            description = "",
+            status = "created",  # créé, en cours, terminé,
+
+    ):
+
+
+    def create(self):
         pass
 
-    def matchs(self):
+    def generate_matchs(self):
         pass
 
-    def save(self):
+    def remove_one(self):
+        pass
+
+    def update(self):
+        pass
+
+    @classmethod
+    def remove_all(cls):
+        pass
+
+    @classmethod
+    def find_one(cls):
+        pass
+
+    @classmethod
+    def find_all(cls):
+        pass
+
+    def add_player(self):
+        if status == created :
+            add
+
+
+
+
+        if not player.ine in players_list:
+            add
+
+
+
+        if self.len(players_list) == MAX_PLAYERS:
+            ghg
+        pass
+
+    def add_players(self):
+        pass
+
+    def __repr__(self):
         pass
